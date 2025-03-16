435863eb0d66ef79ad0d4813544dd73072cfec53
Swetha-1603 <vinjamuriswetha16@gmail.com>
2025-03-15 22:26:56 -0500

    justpractice

--- backend.sh
+++ backend.sh
@@ -31,7 +31,7 @@ unzip /tmp/backend.zip
 
 npm install &>>$LOGFILE
 
-cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 systemctl daemon-reload &>>$LOGFILE
 
